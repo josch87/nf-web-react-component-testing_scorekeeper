5536449856edf1981cbc9634d817959699197984
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-21 12:42:07 +0200

    test navigation

--- components/Navigation/Navigation.test.js
+++ components/Navigation/Navigation.test.js
@@ -7,4 +7,18 @@ jest.mock("next/router", () => ({
   },
 }));
 
-test("renders with two links 'Play' and 'History'", () => {});
+test("renders with two links 'Play' and 'History'", () => {
+  render(<Navigation />);
+
+  const playButton = screen.getByRole("link", {
+    name: /play/i,
+  });
+
+  expect(playButton).toBeInTheDocument();
+
+  const historyButton = screen.getByRole("link", {
+    name: /history/i,
+  });
+
+  expect(historyButton).toBeInTheDocument();
+});
